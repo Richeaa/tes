39955ad2690a2b3f33c8b6830f78207ecab0dd66
Rich Anthonios <rich.anthonios@student.president.ac.id>
2025-12-04 15:41:54 +0700

    Add Jenkinsfile for CI/CD pipeline

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ function add(a, b) {
   return a + b;
 }
 
-module.exports = { add };
+function subtract(a, b) {
+  return a - b;
+}
+
+module.exports = { add, subtract };
 console.log("App running...");
+
+console.log("Testing CI/CD pipeline!");
